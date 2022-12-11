6473257b932ec98cac8309ccb2b4d8ba93719a12
Jimmy_Pangalinan <Jimmipangalinan@gmail.com>
2022-12-11 22:51:39 +0700

    update greatings

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! JIMMY HERE');
+  res.send('Hello World! JIMMY HERE ges');
 });
 
 var server = app.listen(3000, function () {
