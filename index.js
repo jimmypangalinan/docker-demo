24a24f0f74b556f1c60dfd38e6e0c41080a0b2a1
jimmypangalinan <jimmipangalinan@gmail.com>
2022-12-13 08:01:49 +0700

    add 02

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!! image test-001');
+  res.send('Hello World!! image test-002');
 });
 
 var server = app.listen(3000, function () {
